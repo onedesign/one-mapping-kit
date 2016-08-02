6ab3b1284611a9a5dff13cd38611dfe11fa1815a
thelowlypeon <me@petercompernolle.com>
2016-08-02 13:14:38 -0500

    adds mapping kit xcode project

---
+++ OneMappingKit/OneMappingKit.h
@@ -0,0 +1,19 @@
+//
+//  OneMappingKit.h
+//  OneMappingKit
+//
+//  Created by Peter Compernolle on 8/2/16.
+//  Copyright © 2016 One Design Company, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneMappingKit.
+FOUNDATION_EXPORT double OneMappingKitVersionNumber;
+
+//! Project version string for OneMappingKit.
+FOUNDATION_EXPORT const unsigned char OneMappingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneMappingKit/PublicHeader.h>
+
+
